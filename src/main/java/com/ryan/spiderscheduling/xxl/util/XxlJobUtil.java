23ba4b06c320fe7aa1e686b9418f6cff6e55cae0
富沂 <1312170600@qq.com>
2020-12-31 18:02:29 +0800

    Initial commit

---
+++ src/main/java/com/ryan/spiderscheduling/xxl/util/XxlJobUtil.java
@@ -0,0 +1,21 @@
+package com.ryan.spiderscheduling.xxl.util;
+
+import com.xxl.job.core.biz.AdminBiz;
+import com.xxl.job.core.biz.client.AdminBizClient;
+import com.xxl.job.core.biz.model.HandleCallbackParam;
+import com.xxl.job.core.biz.model.RegistryParam;
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.enums.RegistryConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 封装RestFul API 操作
+ */
+
+public class XxlJobUtil {
+
+
+}
---
+++ src/test/java/com/ryan/spiderscheduling/SpiderSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.spiderscheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiderSchedulingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
